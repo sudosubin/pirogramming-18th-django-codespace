52f0c3abfc5fa48ad41f8f2e16d587970b67bd0f
sudosubin <sudosubin@gmail.com>
2023-01-12 11:57:15 +0900

    Posts 모델 수정

--- server/apps/posts/models.py
+++ server/apps/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=32)
+    title = models.CharField(max_length=64)
     user = models.CharField(max_length=32)
     content = models.TextField()
     region = models.CharField(max_length=16)
